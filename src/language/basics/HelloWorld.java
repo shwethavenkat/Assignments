63f3afd7b2a0672ff5f8b1c4edbb3769d04b1fb2
Shwethavenkat <swetha.gundla@gmail.com>
2025-09-26 08:59:03 +0530

    Assignments

---
+++ src/language/basics/HelloWorld.java
@@ -0,0 +1,12 @@
+package language.basics;
+
+public class HelloWorld {
+	
+//This is a main method, JVM begin the execution from main method
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
